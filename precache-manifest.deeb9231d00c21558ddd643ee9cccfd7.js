11a39e1691fbeeb6e39986bc1d304988e8e32588
Bryant Rivas <bryantrivas1127@yahoo.com>
2018-10-31 14:55:43 -0400

    Updates

---
+++ precache-manifest.deeb9231d00c21558ddd643ee9cccfd7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ff1ce2e99aa4e0e52a62",
+    "url": "/static/js/main.ff1ce2e9.chunk.js"
+  },
+  {
+    "revision": "e1ada48e35de0e3b8be4",
+    "url": "/static/js/1.e1ada48e.chunk.js"
+  },
+  {
+    "revision": "ff1ce2e99aa4e0e52a62",
+    "url": "/static/css/main.20ee30ff.chunk.css"
+  },
+  {
+    "revision": "a76d7cdeb92a84d80df7545e56c55a78",
+    "url": "/index.html"
+  }
+];
